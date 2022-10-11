bc9cce9d1109aadc7666af4032b6192be6efded8
Scotch <horsinaround235@gmail.com>
2022-10-11 12:18:12 -0400

    added other possible employees and test code

---
+++ __tests__/Manager.test.js
@@ -0,0 +1,19 @@
+const Manager = require("../lib/Manager");
+
+test("Set office number via constructor argument", () => {
+  const testValue = 100;
+  const e = new Manager("Wilson", 1, "test@test.com", testValue);
+  expect(e.officeNumber).toBe(testValue);
+});
+
+test("getRole() should return \"Manager\"", () => {
+  const testValue = "Manager";
+  const e = new Manager("Wilson", 1, "test@test.com", 100);
+  expect(e.getRole()).toBe(testValue);
+});
+
+test("Get office number via getOffice()", () => {
+  const testValue = 100;
+  const e = new Manager("Wilson", 1, "test@test.com", testValue);
+  expect(e.getOfficeNumber()).toBe(testValue);
+});
